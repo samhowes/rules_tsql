5bad9af967704c10c80eae8f7823fc6d0e8d538a
samhowes <sam@samhowes.com>
2021-11-03 00:02:32 -0400

    runfiles for windows

--- tests/tools/build_test/build_test.sh
+++ tests/tools/build_test/build_test.sh
@@ -11,6 +11,6 @@ source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
 # --- end runfiles.bash initialization v2 ---
 
-runner="$(rlocation "$1")"
+runner="$(rlocation "rules_tsql/$1")"
 
 $runner "$2" "$3"
